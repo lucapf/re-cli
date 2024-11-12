44c1efcd28be4dee660211a7f9fa514dd2233af0
luca <lucapf@gmail.com>
2024-11-12 08:58:05 +0400

    completed fetching from propertyfinder and pulse download transaction file. Need to complete the insert transaction in the database

---
+++ cli.py
@@ -0,0 +1,14 @@
+from reveal import logging, propertyfinder, pulse
+
+
+
+if __name__ == "__main__":
+   # propertyfinder.get_ads(10) 
+   # filename = pulse.download_transaction()
+   # logging.info(f"transactions available in the file {filename}")
+  ## TODO: 
+  ## check the new transaction file is up to dated
+  ## change the logic for the save. Use on conflict to nothing and count before and after 
+
+
+
